73405a30166f599b37a58612f898a673c758a21c
AlinaLisenkova <pomoshnikalina15@gmail.com>
2021-03-07 11:50:32 +0900

    lesson1 an exercise 1

---
+++ 1_Lesson1_LisenkovaAlina/1_Lesson1_LisenkovaAlina/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  1_Lesson1_LisenkovaAlina
+//
+//  Created by AlinaLisenkova on 07.03.2021.
+//
+
+import Foundation
+
+print("Привет! Знаешь решение уравнения X=3^2-4*1*2?")
+
+var a:Double = 1;
+var b:Double = 3;
+var c:Double = 2;
+var d:Double = (pow(b, 2)) * a * c;
+print (d)
